5652daeb66d5e1f71b6ba66dad0d2a2d5fbe39b2
Greg Watson <g.watson@computer.org>
2015-05-28 09:40:30 -0400

    Bug 468629 - Get "Show Terminal" menu working again
    Change-Id: I909e2bc9eb8d4fd626c202dcc5da2db0da1ac215
    Signed-off-by: Greg Watson <g.watson@computer.org>

--- core/org.eclipse.ptp.remote.terminal/src/org/eclipse/ptp/internal/remote/terminal/TerminalCommandHandler.java
+++ core/org.eclipse.ptp.remote.terminal/src/org/eclipse/ptp/internal/remote/terminal/TerminalCommandHandler.java
@@ -18,16 +18,12 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.remote.core.IRemoteConnection;
-import org.eclipse.remote.core.IRemoteConnectionHostService;
-import org.eclipse.remote.core.exception.RemoteConnectionException;
-import org.eclipse.tm.internal.terminal.provisional.api.ITerminalConnector;
-import org.eclipse.tm.internal.terminal.provisional.api.TerminalConnectorExtension;
+import org.eclipse.tm.terminal.connector.remote.IRemoteSettings;
 import org.eclipse.tm.terminal.view.core.TerminalServiceFactory;
 import org.eclipse.tm.terminal.view.core.interfaces.ITerminalService;
 import org.eclipse.tm.terminal.view.core.interfaces.constants.ITerminalsConnectorConstants;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-@SuppressWarnings("restriction")
 public class TerminalCommandHandler extends AbstractHandler {
 
 	@Override
@@ -42,52 +38,32 @@ public class TerminalCommandHandler extends AbstractHandler {
 		return null;
 	}
 
-	private static Map<String, ITerminalConnector> cons = new HashMap<String, ITerminalConnector>();
-
-	/**
-	 * Problems occur if we try to open two connections to the same machine. Keep track
-	 * of what's alive and don't open it twice.
-	 * 
-	 * @param irc
-	 * @return
-	 * @throws RemoteConnectionException
-	 */
-	private static synchronized ITerminalConnector getConnector(IRemoteConnection irc) throws RemoteConnectionException {
-		IRemoteConnectionHostService hostSvc = irc.getService(IRemoteConnectionHostService.class);
-		ITerminalConnector con = cons.get(hostSvc.getHostname());
-		if (con == null) {
-			con = TerminalConnectorExtension.makeTerminalConnector("org.eclipse.tm.terminal.RemoteConnector"); //$NON-NLS-1$
-			cons.put(hostSvc.getHostname(), con);
-		}
-		return con;
-	}
-
 	private void connector(IProject prj) {
 		IRemoteConnection irc = Util.getRemoteConnection(prj);
-		if (irc == null) {
-			return;
-		}
+		if (irc != null) {
+			// Define the terminal properties
+			Map<String, Object> properties = new HashMap<String, Object>();
+			properties.put(IRemoteSettings.REMOTE_SERVICES, irc.getConnectionType().getId());
+			properties.put(IRemoteSettings.CONNECTION_NAME, irc.getName());
 
-		IRemoteConnectionHostService hostSvc = irc.getService(IRemoteConnectionHostService.class);
+			properties.put(ITerminalsConnectorConstants.PROP_DELEGATE_ID,
+					"org.eclipse.tm.terminal.connector.telnet.launcher.remote"); //$NON-NLS-1$
+			properties.put(ITerminalsConnectorConstants.PROP_ENCODING, "UTF-8"); //$NON-NLS-1$
+			properties.put(ITerminalsConnectorConstants.PROP_PROCESS_WORKING_DIR, "/tmp"); //$NON-NLS-1$
 
-		// Define the terminal properties
-		Map<String, Object> properties = new HashMap<String, Object>();
-		properties.put(ITerminalsConnectorConstants.PROP_TITLE, irc.getName());
-		properties.put(ITerminalsConnectorConstants.PROP_ENCODING, "UTF-8"); //$NON-NLS-1$
-		properties.put(ITerminalsConnectorConstants.PROP_PROCESS_WORKING_DIR, "/tmp"); //$NON-NLS-1$
+			// Create the done callback object
+			ITerminalService.Done done = new ITerminalService.Done() {
+				@Override
+				public void done(IStatus done) {
+					// Place any post processing here
+				}
+			};
 
-		// Create the done callback object
-		ITerminalService.Done done = new ITerminalService.Done() {
-			@Override
-			public void done(IStatus done) {
-				// Place any post processing here
+			// Open the terminal
+			ITerminalService terminal = TerminalServiceFactory.getService();
+			if (terminal != null) {
+				terminal.openConsole(properties, done);
 			}
-		};
-
-		// Open the terminal
-		ITerminalService terminal = TerminalServiceFactory.getService();
-		if (terminal != null) {
-			terminal.openConsole(properties, done);
 		}
 	}
 }
